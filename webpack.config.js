19c0c4a9e76d3ef8fd25d389ea8f39d8c8e27a8c
Tom Van Laerhoven <tom.vanlaerhoven@gmail.com>
2023-12-22 15:52:19 +0100

    Add support for web

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
+// THEOplayer's libraryLocation.
+const libraryLocation = 'theoplayer';
+const projectRoot = path.resolve(__dirname, '.');
+
+const CopyWebpackPluginConfig = new CopyWebpackPlugin({
+  patterns: [
+    {
+      // Copy transmuxer worker files.
+      // THEOplayer will find them by setting `libraryLocation` in the playerConfiguration.
+      from: path.resolve(projectRoot, './node_modules/theoplayer/THEOplayer.transmux.*').replace(/\\/g, '/'),
+      to: `${libraryLocation}/[name][ext]`,
+    },
+    {
+      // Copy CSS files
+      from: path.resolve(projectRoot, './web/public/*.css').replace(/\\/g, '/'),
+      to: `[name][ext]`,
+    },
+  ],
+});
+
+module.exports = async function (env, argv) {
+  const config = await createExpoWebpackConfigAsync(env, argv);
+
+  config.plugins.push(CopyWebpackPluginConfig);
+
+  // Customize the config before returning it.
+  return config;
+};
